1d3c1fb158cf0d67f813c6fbe8fc7d9ee4cd832d
benjamalegni <lukabenjaminmalegni@gmail.com>
2026-06-27 06:32:33 -0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,24 @@ int main(int argc, char* argv[]) {
         {"model", "anthropic/claude-haiku-4.5"},
         {"messages", json::array({
             {{"role", "user"}, {"content", prompt}}
+        })},
+        {"tools", json::array({
+            {
+            {"type", "function"},
+            {"function", {
+              {"name", "Read"}
+              {"description", "Read and return the contents of a file"}
+              {"parameters", {
+                {"type", "object"},
+                {"properties", {
+                  {"file_path", {
+                    {"type", "string"},
+                    {"description", "The path to the file to read"}
+                  }}
+                }},
+                {"required", {"file_path"}}
+              }
+            }}}
         })}
     };
 
