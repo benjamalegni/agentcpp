c040ddce0bc2093acc24cecd41d49653f229b863
benjamalegni <lukabenjaminmalegni@gmail.com>
2026-06-27 06:21:37 -0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -62,8 +62,7 @@ int main(int argc, char* argv[]) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     std::cerr << "Logs from your program will appear here!" << std::endl;
 
-    // TODO: Uncomment the line below to pass the first stage
-    // std::cout << result["choices"][0]["message"]["content"].get<std::string>();
+    std::cout << result["choices"][0]["message"]["content"].get<std::string>();
 
     return 0;
 }
