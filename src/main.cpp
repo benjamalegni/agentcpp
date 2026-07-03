9a915a57f7b1ba11ef33e7f963829708a689951f
benjamalegni <lukabenjaminmalegni@gmail.com>
2026-07-03 12:24:49 -0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,8 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    try {
         auto prompt = parse_prompt(argc, argv);
         auto config = load_config();
 
         return run_agent(config, prompt);
-    } catch (const std::exception& exception) {
-        std::cerr << exception.what() << std::endl;
-        return 1;
-    }
 }
